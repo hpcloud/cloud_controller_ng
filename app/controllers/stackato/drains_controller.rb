ce6ee29138cc2b706c0e838eb1968cc6a9ca0206
Phil Whelan <phil123@gmail.com>
2013-09-10 16:07:28 -0700

    updates to /v2/stackato/logyard (now "drains")
    make more RESTful

---
+++ app/controllers/stackato/drains_controller.rb
@@ -0,0 +1,56 @@
+
+require "kato/logyard"
+
+module VCAP::CloudController
+  class StackatoDrainsController < RestController::Base
+
+    def list
+      raise Errors::NotAuthorized unless roles.admin?
+      drain_uris = Kato::Logyard.run_logyard_remote "list", []
+      drain_statuses = Kato::Logyard.run_logyard_remote "status", []
+      drain_hash = {}
+      if drain_uris
+        drain_uris.each do |drain_name, uri|
+          drain_hash[drain_name] ||= {}
+          drain_hash[drain_name][:uri] = uri
+        end
+      end
+      if drain_statuses
+        drain_statuses.each do |drain_name, status|
+          drain_hash[drain_name] ||= {}
+          drain_hash[drain_name][:status] = status
+        end
+      end
+      drain_list = []
+      drain_hash.each do |drain_name, drain|
+        drain[:name] = drain_name
+        drain_list << drain
+      end
+      Yajl::Encoder.encode(drain_list)
+    end
+
+    def add
+      raise Errors::NotAuthorized unless roles.admin?
+      new_logyard = Yajl::Parser.parse(body)
+      logger.info("Adding drain with args: #{new_logyard}")
+      response = Kato::Logyard.run_logyard_remote "add", new_logyard
+      Yajl::Encoder.encode(response)
+    end
+
+    def delete(name)
+      raise Errors::NotAuthorized unless roles.admin?
+      logger.info("Deleting drain '#{name}'")
+      response = Kato::Logyard.run_logyard_remote "delete", [name]
+      Yajl::Encoder.encode(response)
+    end
+
+    # TODO:Stackato: We need to limit scope and define what this API is.
+    #                "Anything that logyard-cli accepts" is too broad.
+
+    # Exposing logyard-cli/logyard-remote via CC API
+    get    "/v2/stackato/drains",       :list
+    post   "/v2/stackato/drains",       :add
+    delete "/v2/stackato/drains/:name", :delete
+
+  end
+end
