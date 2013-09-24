f3bd2f2b7cba73ad136acf0b7b1e3981e4291af9
Phil Whelan <phil123@gmail.com>
2013-09-23 17:34:13 -0700

    add ssh_key retrieve to ccng api

---
+++ app/controllers/stackato/ssh_key.rb
@@ -0,0 +1,19 @@
+
+require "kato/config"
+
+module VCAP::CloudController
+  class StackatoSshKeyController < RestController::Base
+
+    def get_ssh_key
+      ssh_key = Kato::Config.get("cluster", "stackato_ssh_keypair_alluser/privkey")
+      if ssh_key
+        Yajl::Encoder.encode(ssh_key)
+      else
+        raise Errors::StackatoSshKeyNotConfigured.new
+      end
+    end
+
+    get "/v2/stackato/ssh_key", :get_ssh_key
+
+  end
+end
