9ce368946d92fb3550ebc8141e38975964ca40cb
Phil Whelan <phil123@gmail.com>
2013-09-23 17:51:34 -0700

    temp allow_unauthenticated_access on ssh_key
    stackato client does not pass auth token
    
    not a huge security issue, since all users share the same
    ssh key, but should limit to visibility to logged-in users.

--- app/controllers/stackato/ssh_key.rb
+++ app/controllers/stackato/ssh_key.rb
@@ -3,6 +3,9 @@ require "kato/config"
 
 module VCAP::CloudController
   class StackatoSshKeyController < RestController::Base
+    # TODO:Stackato: Remove unauthorized access when stackato
+    #                client passes auth token.
+    allow_unauthenticated_access
 
     def get_ssh_key
       ssh_key = Kato::Config.get("cluster", "stackato_ssh_keypair_alluser/privkey")
