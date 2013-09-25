dc54b9e78250618ba7cc8448e7f4a6887446bdce
Phil Whelan <phil123@gmail.com>
2013-09-25 16:02:27 -0700

    add cloudevents api to ccng

---
+++ app/controllers/stackato/cloudevents_controller.rb
@@ -0,0 +1,39 @@
+
+require 'digest/md5'
+require "cloud_controller/stackato/redis_client"
+
+module VCAP::CloudController
+  class StackatoCloudeventsController < RestController::Base
+    # TODO:Stackato remove this line
+    allow_unauthenticated_access
+
+    def get_cloudevents
+      num = params[:n].to_i
+      num = 25 unless num > 0
+      events_list =  redis { |r| r.lrange("cloud_events", 0, num-1) }
+      since_md5 = params[:since_md5]
+
+      ignore = false
+      filtered_events = []
+      events_list.each do |event|
+        digest = Digest::MD5.hexdigest(event)
+        if since_md5 == digest
+          # this and all following events have been seen already.
+          ignore = true
+        end
+        next if ignore
+        event_hash = Yajl::Parser.parse(event)
+        event_hash[:md5] = digest
+        filtered_events << event_hash
+      end
+      Yajl::Encoder.encode({ :results => filtered_events })
+    end
+
+    def redis &block
+      StackatoRedisClient.redis &block
+    end
+
+    get "/v2/stackato/cloudevents", :get_cloudevents
+
+  end
+end
