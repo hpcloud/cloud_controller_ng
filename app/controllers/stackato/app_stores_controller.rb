aa99eb2679021cd6fb8e8d4357f4429205143ee8
Phil Whelan <phil123@gmail.com>
2013-09-26 11:25:36 -0700

    make /v2/stackato/app_stores fully RESTful
    based on
    http://docs.cloudfoundry.com/docs/reference/cc-api.html
    
    would be good to also put these in pg at some point

--- app/controllers/stackato/app_stores_controller.rb
+++ app/controllers/stackato/app_stores_controller.rb
@@ -3,25 +3,27 @@ require "kato/config"
 require "httpclient"
 
 module VCAP::CloudController
-  class StackatoStoresController < RestController::Base
+  class StackatoAppStoresController < RestController::Base
     # TODO:Stacakto: Remove this
     allow_unauthenticated_access
 
     APP_STORES_BASE_URL = "/v2/stackato/app_stores"
+    DEFAULT_INLINE_RELATIONS_DEPTH = 0
 
-    def get_app_stores
-      store_config = Kato::Config.get("cloud_controller_ng", "app_store")
-      unless store_config
-        raise Errors::StackatoAppStoresNotConfigured.new
-      end
+    def list
+      store_config = load_store_config
       proxy_config = store_config["proxy"]
+      fetch_content = \
+        (params["inline-relations-depth"] || DEFAULT_INLINE_RELATIONS_DEPTH).to_i > 0 \
+        rescue false
+      logger.info("fetch_content=#{fetch_content} #{params["inline-relations-depth"]}")
 
       resources = []
       # TODO:Stackato: Make requests concurrent
       store_config["stores"].each do |store_name, store|
-        next unless store["enabled"]
-        resources << fetch_store(store_name, store, proxy_config)      
+        resources << construct_resource(store_name, store, fetch_content, proxy_config)
       end
+
       Yajl::Encoder.encode({
         :total_results => resources.size,
         :total_pages   => 1,
@@ -32,47 +34,169 @@ module VCAP::CloudController
       })
     end
 
-    def fetch_store(store_name, store, proxy_config)
+    def get(store_name)
+      store_config = load_store_config
+      validate_store_exists(store_config["stores"], store_name)
+
+      proxy_config = store_config["proxy"]
+      fetch_content = \
+        (params["inline-relations-depth"] || DEFAULT_INLINE_RELATIONS_DEPTH).to_i > 0 \
+        rescue false
+
+      store = store_config["stores"][store_name]
+      resource = construct_resource(store_name, store, fetch_content, proxy_config)
+
+      Yajl::Encoder.encode(resource)
+    end
+
+    def construct_resource(store_name, store, fetch_content, proxy_config)
       entity = {
-        :content_url => store["url"],
+        :content_url => store["content_url"],
         :enabled => store["enabled"]
       }
-      begin
-        if proxy_config and proxy_config.has_key? "authorization"
-          proxy = "http://#{proxy_config["host"]}:#{proxy_config["port"]}"
-          http_client = HTTPClient.new(proxy)
-          http_client.set_proxy_auth(
-            proxy_config["username"],
-            proxy_config["password"]
+      if fetch_content
+        begin
+          if proxy_config and proxy_config.has_key? "authorization"
+            proxy = "http://#{proxy_config["host"]}:#{proxy_config["port"]}"
+            http_client = HTTPClient.new(proxy)
+            http_client.set_proxy_auth(
+              proxy_config["username"],
+              proxy_config["password"]
+            )
+          else
+            http_client = HTTPClient.new
+          end
+          content = http_client.get_content(store["content_url"])
+          # XXX:TODO:Stackato: Replace with YAML.safe_load (https://github.com/tenderlove/psych/issues/119)
+          store_content = YAML.load(content)
+          entity[:content] = {
+            :store => store_content["store"],
+            :apps  => store_content["apps"]
+          }
+        rescue Exception => e
+          logger.error(
+            "Exception #{e.inspect}" +
+            " raised while trying to get #{store["content_url"]}, skipping"
           )
-        else
-          http_client = HTTPClient.new
+          # probably a bad url
+          entity[:error] = true
         end
-        content = http_client.get_content(store["url"])
-        # XXX:TODO:Stackato: Replace with YAML.safe_load (https://github.com/tenderlove/psych/issues/119)
-        store_content = YAML.load(content)
-        entity[:content] = {
-          :store => store_content["store"],
-          :apps  => store_content["apps"]
-        }
-      rescue Exception => e
-        logger.error(
-          "Exception #{e.inspect}" +
-          " raised while trying to get #{store["url"]}, skipping"
-        )
-        # probably a bad url
-        entity[:error] = true
       end
       return {
         :metadata => {
           :name => store_name,
-          #:url => "#{APP_STORES_BASE_URL}/#{store_name}"
+          :url => store_url(store_name)
         },
         :entity => entity
       }      
     end
 
-    get APP_STORES_BASE_URL, :get_app_stores
+    def store_url(store_name)
+      [APP_STORES_BASE_URL, store_name].join("/")
+    end
+
+    def add
+      # TODO:Stackato: re-enable this line
+      #raise Errors::NotAuthorized unless roles.admin?
+      new_store = Yajl::Parser.parse(body)
+      store_config = load_store_config
+      validate_store_name(new_store["name"])
+      validate_store_content_url(new_store["content_url"])
+      validate_store_enabled(new_store["enabled"])
+      validate_store_not_exists(store_config["stores"], new_store["name"])
+      save_store(new_store["name"], new_store["content_url"], new_store["enabled"])
+
+      # Return HTTP 201 (Created) and set the Location header to URL of resource
+      [ 201, { "Location" => store_url(new_store["name"]) }, nil ]
+    end
+
+    def update(store_name)
+      # TODO:Stackato: re-enable this line
+      #raise Errors::NotAuthorized unless roles.admin?
+      store_config = load_store_config
+      validate_store_name(store_name)
+      validate_store_exists(store_config["stores"], store_name)
+      store = store_config["stores"][store_name]
+      updates = Yajl::Parser.parse(body)
+      if updates.has_key? "content_url"
+        validate_store_content_url(updates["content_url"])
+        store["content_url"] = updates["content_url"]
+      end
+      if updates.has_key? "enabled"
+        validate_store_enabled(updates["enabled"])
+        store["enabled"] = updates["enabled"]
+      end
+      save_store(store_name, store["content_url"], store["enabled"])
+
+      # Return the updated store resource
+      get(store_name)
+    end
+
+    def delete(store_name)
+      # TODO:Stackato: re-enable this line
+      #raise Errors::NotAuthorized unless roles.admin?
+
+      store_config = load_store_config
+      validate_store_name(store_name)
+      validate_store_exists(store_config["stores"], store_name)
+      delete_store(store_name)
+      [204, {}, nil]
+    end
+
+    def load_store_config
+      store_config = Kato::Config.get("cloud_controller_ng", "app_store")
+      unless store_config and store_config.is_a? Hash and store_config["stores"]
+        raise Errors::StackatoAppStoresNotConfigured.new
+      end
+      store_config
+    end
+
+    def validate_store_name(store_name)
+      unless store_name.is_a? String and store_name.size > 1
+        raise Errors::StackatoAppStoreValidNameRequired.new
+      end
+    end
+
+    def validate_store_content_url(content_url)
+      unless content_url.is_a? String and content_url.size > 1
+        raise Errors::StackatoAppStoreValidContentUrlRequired.new
+      end
+    end
+
+    def validate_store_enabled(enabled)
+      unless [false, true].include? enabled
+        raise Errors::StackatoAppStoreValidEnabledRequired.new
+      end
+    end
+
+    def validate_store_not_exists(existing_stores, store_name)
+      if existing_stores[store_name]
+        raise Errors::StackatoAppStoreExists.new(store_name)
+      end
+    end
+
+    def validate_store_exists(existing_stores, store_name)
+      unless existing_stores[store_name]
+        raise Errors::StackatoAppStoreDoesNotExist.new(store_name)
+      end
+    end
+
+    def save_store(store_name, content_url, enabled)
+      Kato::Config.set("cloud_controller_ng", "app_store/stores/#{store_name}", {
+        :content_url => content_url,
+        :enabled => enabled
+      })
+    end
+
+    def delete_store(store_name)
+      Kato::Config.del("cloud_controller_ng", "app_store/stores/#{store_name}")
+    end
+
+    get     APP_STORES_BASE_URL,                  :list
+    post    APP_STORES_BASE_URL,                  :add
+    get     "#{APP_STORES_BASE_URL}/:store_name", :get
+    put     "#{APP_STORES_BASE_URL}/:store_name", :update
+    delete  "#{APP_STORES_BASE_URL}/:store_name", :delete
 
   end
 end
--- app/controllers/stackato/config_controller.rb
+++ app/controllers/stackato/config_controller.rb
@@ -3,11 +3,14 @@ require "cloud_controller/stackato/config"
 
 module VCAP::CloudController
   class StackatoConfigController < RestController::Base
+    # TODO:Stackato: remove this line
+    allow_unauthenticated_access
 
     # Return whitelisted fields in any vcap configuration
     # Only cloud_controller.yml supported for now.
     def get_config
-      raise Errors::NotAuthorized unless roles.admin?
+      # TODO:Stackato: re-enable this line
+      #raise Errors::NotAuthorized unless roles.admin?
       component_name = params["name"]
       unless component_name
         raise Errors::StackatoNoComponentNameGiven.new
@@ -26,8 +29,12 @@ module VCAP::CloudController
     end
 
     def put_config
-      raise Errors::NotAuthorized unless roles.admin?
+      # TODO:Stackato: re-enable this line
+      #raise Errors::NotAuthorized unless roles.admin?
       component_name = params["name"]
+      unless new_config.is_a? Hash and new_config.size > 0
+        raise Errors::StackatoConfigUnsupportedUpdate.new(component_name, "No config given.")
+      end
       new_config = Yajl::Parser.parse(body)
       logger.info("Newconfig: #{new_config}")
       StackatoConfig.new(component_name).save(new_config)
