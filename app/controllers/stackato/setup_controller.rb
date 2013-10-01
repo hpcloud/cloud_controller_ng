dbb25c34052ee05325c3652a56b8ce4c4bb19738
Andrew Cole <aocole@gmail.com>
2013-09-30 18:35:25 -0700

    first user creation endpoint

--- app/controllers/stackato/setup_controller.rb
+++ app/controllers/stackato/setup_controller.rb
@@ -3,94 +3,16 @@ require "kato/config"
 require "kato/local/util"
 
 module VCAP::CloudController
-  class StackatoSetupController < RestController::Base
+  rest_controller :StackatoSetup do
     allow_unauthenticated_access
+    disable_default_routes
+    include StackatoUserCreation
+    do_define_attributes true
+    define_messages
 
-    # One-time setup done without any authen/authz
-    # 1. Change the UNIX password (from the default 'stackato')
-    # 2. Create the first user
-    # 3. Mark this user as an admin
-    #    a. in the YAML file
-    #    b. in the internal admins list
     def setup
-      user = nil
-
-      user_data = Yajl::Parser.parse(body)
-
-      if Kato::Config.get("cluster", "license")
-        raise Errors::StackatoLicenseAlreadySetup.new
-      elsif Kato::Config.get("aok", "enabled") && !aok_users_enabled?
-        raise Errors::StackatoConsoleSetupRequiresAokBuiltinUserDB.new
-
-      # Validation
-      # TODO: Move validation to the user model & determine 
-      # what we can really enforce in light of AOK
-      elsif not (user_data["email"])
-        raise Errors::StackatoSetupEmailRequired.new
-      elsif user_data["email"].length < 6
-        raise Errors::StackatoSetupEmailMinLength.new(6)
-      elsif user_data["password"] && user_data["password"].length <= 4 or
-          (user_data["unix_password"] and user_data["unix_password"].length <= 4)
-        raise Errors::StackatoSetupPasswordMinLength.new(4)
-      else
-
-        run_cmd('sudo -k', "failed to reset sudo password cache")
-
-        # TODO:Stackato: Define transaction correctly
-        #Sequel::Model.db.transaction do
-        begin
-          logger.info("SETUP: creating admin user #{user_data["email"]}")
-
-          # TODO:Stackato: Figure out how to create user. Should we
-          #                be creating a user here?
-          #user = create_user(user_data["email"], user_data["password"], true, :check_admin => false)
-          # ???
-          #user = Models::User.create(
-          #  :id => user_data["email"], :provider => provider
-          #)
-
-          # Change the unix password, assuming 'stackato' as the current password
-          if user_data["unix_password"] and Kato::Local::Util.password_is_stackato?
-            logger.info("SETUP: changing unix user password")
-            # passwords may contain shell meta-characters; so write to a
-            # tmp file and then pipe its contents to chpasswd
-            passwdfile = '/tmp/cc-setup-p-tmp'
-            File.open(passwdfile, 'w') do |f|
-              f.puts("stackato:#{user_data["unix_password"]}")
-            end
-            cmd = "echo 'stackato' | sudo -S sh -c \"cat #{passwdfile} | chpasswd\""
-            run_cmd(cmd, "failed to change stackato unix password")
-            File.delete(passwdfile)
-          end
-
-          # from this point, only an admin should be able to create new users
-          logger.info("SETUP: disabling user registration")
-          Kato::Config.set("cloud_controller_ng", "allow_registration", false)
-
-        end # transaction
-
-        logger.info("SETUP: storing the license key in config")
-        Kato::Config.set("cluster", "license", "type: microcloud")
-
-        # TODO:Stackato encode the created usertoken if the return value is used anywhere
-        #Yajl::Encoder.encode(UserToken.create(user.email))
-        Yajl::Encoder.encode({})
-      end
-    end
-
-    # Run a command and report to log, and back to caller.
-    # The STDOUT of the cmd will only be printed to the log.
-    @private
-    def run_cmd(cmd, errmsg)
-      output = `#{cmd}`
-      if not $?.success?
-        logger.info("SETUP: #{errmsg}: #{output}")
-        # TODO:Stackato: Handle in a more ccng way
-        #raise CloudError.new(CloudError::CONSOLE_GENERIC, errmsg)
-      end
-    end
-
-    post "/v2/stackato/license", :setup
+      create_core(:is_first_user_setup => true)
+    end; post "/v2/stackato/firstuser", :setup
 
   end
 end
--- app/controllers/stackato/users_controller.rb
+++ app/controllers/stackato/users_controller.rb
@@ -1,24 +1,13 @@
 require "uaa/token_issuer"
 require "uaa/scim"
+require_relative 'stackato_user_creation'
 module VCAP::CloudController
   rest_controller :StackatoUsers do
+    include StackatoUserCreation
     ADMIN_GROUP = "cloud_controller.admin"
     path_base('stackato/users')
 
-    # There's a model() method to do this but it breaks things
-    def model; User; end
-
-    # Attributes defined here control what gets extracted from
-    # the POST body during CreateMessage instantiation.
-    define_attributes do
-      attribute :username, String
-      attribute :given_name, String, :default => nil
-      attribute :family_name, String
-      attribute :email, Message::EMAIL
-      attribute :phone, String
-      attribute :password, String, :exclude_in => [:read, :enumerate]
-      attribute :admin, Message::Boolean, :default => false
-    end
+    do_define_attributes
 
     # def self.translate_validation_exception(e, attributes)
     #   guid_errors = e.errors.on(:guid)
@@ -33,89 +22,7 @@ module VCAP::CloudController
       raise Errors::NotAuthenticated unless user
       raise Errors::NotAuthorized unless roles.admin?
 
-
-      json = body.read
-      logger.debug "JSON POST body: #{json.inspect}"
-      json_msg = self.class::CreateMessage.decode(json)
-      @request_attrs = json_msg.extract(:stringify_keys => true)
-      logger.debug "Request Atts: #{request_attrs.inspect}"
-      user_info = {
-        :userName => request_attrs["username"],
-        :name => {
-          :givenName => request_attrs["given_name"],
-          :familyName => request_attrs["family_name"]
-        },
-        :emails => [
-          :value => request_attrs["email"] #TODO: support multiple emails
-        ],
-        :phoneNumbers => [
-          :value => request_attrs["phone"] #TODO: support multiple phones
-        ],
-        :password => request_attrs["password"]
-      }
-
-      logger.debug "User info to post to UAA: #{user_info.inspect}"
-
-      target = Kato::Config.get("cloud_controller_ng", 'uaa/url')
-
-      #TODO: generate this, retrieve it from config
-      secret = 'cloudcontrollersecret'
-
-      token_issuer =
-        CF::UAA::TokenIssuer.new(target, 'cloud_controller', secret)
-
-
-      token = token_issuer.client_credentials_grant
-
-      scim_client = CF::UAA::Scim.new(target, token.auth_header)
-
-
-      before_create
-      obj = nil
-      model.db.transaction do
-        scim_user = scim_client.add :user, user_info
-        logger.debug "Response from SCIM user creation: #{scim_user.inspect}"
-
-        if request_attrs["admin"]
-          scim_group = scim_client.query( :group, 'filter' => %Q!displayName eq "#{ADMIN_GROUP}"!, 'startIndex' => 1)["resources"].first
-          group_guid = scim_group["id"]
-          members = scim_group["members"].collect{|hash|hash["value"]}
-          members << scim_user['id']
-          group_info = {
-            "id" => group_guid,
-            "schemas" => scim_group['schemas'],
-            "members" => members,
-            "meta" => scim_group['meta'],
-            "displayName" => ADMIN_GROUP
-          }
-          logger.debug "updated group info to put: #{group_info.inspect}"
-          scim_client.put :group, group_info
-        end
-
-        cc_user_info = {
-          'guid' => scim_user['id'],
-          'admin' => request_attrs["admin"] # Should I extract this from scim_user?
-        }
-        obj = model.create_from_hash(cc_user_info)
-        validate_access(:create, obj, user, roles)
-      end
-
-      after_create(obj)
-
-      [ HTTP::CREATED,
-        { "Location" => "#{UsersController.path}/#{obj.guid}" },
-        serialization.render_json(UsersController, obj, @opts)
-      ]
-    rescue CF::UAA::InvalidToken => e
-      logger.error "The token for the 'cloud_controller' oauth2 client was
-      not accepted by #{target}. This should never happen."
-      raise e
-    rescue CF::UAA::TargetError => e
-      # Probably a validation error coming from the target.
-      return 400, e.info.to_json
-    rescue Exception => e
-      #TODO: Rollback UAA user creation.
-      raise e
+      create_core
     end
 
     def enumerate
