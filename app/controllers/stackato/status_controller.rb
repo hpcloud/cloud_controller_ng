8518db8d65f25c7d3215bd64f4102331dc0aedcc
Phil Whelan <phil123@gmail.com>
2013-11-01 16:02:30 -0700

    remove openness in API, which pre-dated proper auth

--- app/controllers/stackato/status_controller.rb
+++ app/controllers/stackato/status_controller.rb
@@ -3,8 +3,6 @@ require "kato/cluster/status"
 
 module VCAP::CloudController
   class StackatoStatusController < RestController::Base
-    # TODO:Stacakto: Remove this
-    allow_unauthenticated_access
 
     def get_status
       Yajl::Encoder.encode(Kato::Cluster::Status.get_status)
