6e0fd9a7ab8393b633e5c3607f8bffeedf6e60c8
Sridhar Ratnakumar <sridharr@activestate.com>
2013-11-21 22:12:06 -0800

    pass buildpack parameter from console to appstore
    Bug #101454

--- app/controllers/stackato/appstore_controller.rb
+++ app/controllers/stackato/appstore_controller.rb
@@ -31,6 +31,7 @@ module VCAP::CloudController
         :AppName => body_params["app_name"],
         :Space => body_params["space_guid"],
         :Url => body_params["url"],
+        :Buildpack => body_params["buildpack"],
         :Token => auth_token_header,
         :VcsUrl => body_params["from"],
         :VcsRef => body_params["commit"],
