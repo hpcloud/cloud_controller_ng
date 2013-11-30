45b266af2818da673b07dbf48451f127fdea629e
Sridhar Ratnakumar <sridharr@activestate.com>
2013-11-29 23:30:10 -0800

    fuck ruby class variable confusion, and violate DRY

--- lib/cloud_controller/stackato/redis_client.rb
+++ lib/cloud_controller/stackato/redis_client.rb
@@ -1,13 +1,16 @@
 
 module VCAP::CloudController
-  class RedisClient
-    def self.configure_base(config, key)
+  class EphemeralRedisClient
+    def self.configure(config)
       @@cc_config = config
-      @@config_key = key
     end
 
     def self.logger
-      @@logger ||= Steno.logger("cc.stackato.redis_client.#{@@config_key}")
+      @@logger ||= Steno.logger("cc.stackato.redis_client.#{config_key}")
+    end
+
+    def self.config_key
+      :ephemeral_redis
     end
 
     def self.mutex
@@ -24,7 +27,7 @@ module VCAP::CloudController
         # connection.
         @@redis ||= nil
         unless @@redis and @@redis.client.connected?
-          redis_config = @@cc_config[@@config_key]
+          redis_config = @@cc_config[config_key]
           unless redis_config.is_a? Hash and redis_config[:host] and redis_config[:port]
             raise Errors::StackatoRedisClientNotConfigured.new
           end
@@ -45,17 +48,51 @@ module VCAP::CloudController
     end
   end
 
-  class EphemeralRedisClient < RedisClient
-    CC_CONFIG_KEY = :ephemeral_redis
+  class AppLogsRedisClient
     def self.configure(config)
-      configure_base(config, CC_CONFIG_KEY)
+      @@cc_config = config
     end
-  end
 
-  class AppLogsRedisClient < RedisClient
-    CC_CONFIG_KEY = :app_logs_redis
-    def self.configure(config)
-      configure_base(config, CC_CONFIG_KEY)
+    def self.logger
+      @@logger ||= Steno.logger("cc.stackato.redis_client.#{config_key}")
+    end
+
+    def self.config_key
+      :app_logs_redis
+    end
+
+    def self.mutex
+      @@mutex ||= Mutex.new
+    end
+
+    def self.redis
+      # Only allow one thread to use the redis client at a time.
+      # Redis processes requests serially, so no benefit to having more than
+      # a single connection to Redis.
+      mutex.synchronize do
+
+        # Create connection to Redis, if we do not already have an active
+        # connection.
+        @@redis ||= nil
+        unless @@redis and @@redis.client.connected?
+          redis_config = @@cc_config[config_key]
+          unless redis_config.is_a? Hash and redis_config[:host] and redis_config[:port]
+            raise Errors::StackatoRedisClientNotConfigured.new
+          end
+          logger.info("Connecting to redis at #{redis_config[:host]}:#{redis_config[:port]}")
+          @@redis = Redis.new(
+            :host => redis_config[:host],
+            :port => redis_config[:port],
+            :password => redis_config[:password]
+          )
+        end
+
+        # Process redis call in a block. This allows us to wrap in mutex, so that
+        # only one thread will use the connection at a time.
+        yield @@redis
+
+      end
+
     end
   end
 
