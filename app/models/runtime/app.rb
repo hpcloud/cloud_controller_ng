hpcloud/cloud_controller_ng

commit 643816cf72e74a417d71e4e4e07dcc07dff8c09c

Let users scale their memory down if they're over their org quota limit. The new behaviour is that they can stop apps if they're over the limit but not start them. And they can decrease an apps memory usage (even if it doesn't bring it below the quota threshold) but not increase it if they're over the limit.

app/models/runtime/app.rb
@@ -107,7 +107,10 @@ module VCAP::CloudController
 
       validate_environment
       validate_metadata
-      check_memory_quota
+
+      if state == "STARTED" || total_requested_memory > total_existing_memory
+        check_memory_quota
+      end
     end
 
     def before_create

commit 83c4d83f69bfbe4a8920850eb091f268daedde8d

fix bug introduced in 643816cf72e74a417d71e4e4e07dcc07dff8c09c by checking if the app is a new app when determining exiosting memory usage

app/models/runtime/app.rb
@@ -272,6 +272,7 @@ module VCAP::CloudController
     end
 
     def total_existing_memory
+      return 0 if new?
       app_from_db = self.class.find(:guid => guid)
       app_from_db[:memory] * app_from_db[:instances]
     end