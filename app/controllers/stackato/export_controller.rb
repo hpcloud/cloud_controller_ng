ca27fd502db142dfa7fb2ee5444b0aaab29ee9fe
Phil Whelan <phil123@gmail.com>
2013-12-05 14:55:27 -0800

    port /v2/stackato/export[_info] from S2.10
    http://bugs.activestate.com/show_bug.cgi?id=101172

---
+++ app/controllers/stackato/export_controller.rb
@@ -0,0 +1,33 @@
+
+require 'stackato/kato_shell'
+
+module VCAP::CloudController
+  class StackatoExportController < RestController::Base
+
+    def export
+      raise Errors::NotAuthorized unless roles.admin?
+      file = KatoShell.export params["regen"]
+      export_filename = 'stackato-export.tgz'
+      if params["hostname"]
+        export_filename = "#{params["hostname"]}-export.tgz"
+      end
+      send_file(
+        file,
+        :filename => export_filename,
+        :type => "application/x-compressed"
+      )
+    end
+
+    def export_info
+      raise Errors::NotAuthorized unless roles.admin?
+      Yajl::Encoder.encode({
+        :export_available   => KatoShell.export_exists?,
+        :export_in_progress => KatoShell.export_in_progress?
+      })
+    end
+
+    get '/v2/stackato/export', :export
+    get '/v2/stackato/export_info', :export_info
+
+  end
+end
