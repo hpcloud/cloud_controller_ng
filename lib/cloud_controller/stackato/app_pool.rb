c862f4a8b3fd4db503c75d6b3a8ddae188bb2c5e
Greg Baguley <gregb@activestate.com>
2014-01-15 15:06:38 +1300

    StackatoAppPool  - Droplet map cleanup
    Changing the data stored in the StackatoAppPool to be more generic and
    provide more information for future tasks (zoning etc)

--- lib/cloud_controller/stackato/app_pool.rb
+++ lib/cloud_controller/stackato/app_pool.rb
@@ -4,8 +4,8 @@ require "steno"
 module VCAP::CloudController
   class StackatoAppPool
     class << self
-      def droplet_map
-        @droplet_map ||= {}
+      def dea_droplet_map
+        @dea_droplet_map ||= {}
       end
 
       def logger
@@ -17,28 +17,36 @@ module VCAP::CloudController
           dea_id = dea_heartbeat['dea']
 
           # If we don't have any droplet info for the dea, init new.
-          if droplet_map[dea_id].nil?
-            droplet_map[dea_id] = {}
+          if dea_droplet_map[dea_id].nil?
+            dea_droplet_map[dea_id] = {'droplets'=>{}}
           end
 
-          dea_droplets = droplet_map[dea_id]
+          dea_droplets = dea_droplet_map[dea_id]['droplets']
 
-          heartbeat_droplets = []
+          heartbeat_droplet_instances = []
 
           # Add the droplet data to the dea
           dea_heartbeat['droplets'].each do |droplet|
+            droplet_id = droplet['droplet']
             droplet_instance = droplet['instance']
-            heartbeat_droplets.push(droplet_instance)
+            heartbeat_droplet_instances.push(droplet_instance)
 
-            if dea_droplets[droplet_instance].nil?
-              dea_droplets[droplet_instance] = droplet
+            if dea_droplets[droplet_id].nil?
+              dea_droplets[droplet_id] = {'instances'=>{}}
             end
+
+            dea_droplets[droplet_id]['instances'][droplet_instance] = droplet
           end
 
           # Remove the droplets that no longer exist in the heartbeat from dea_map
-          dea_droplets.each_pair do |id, droplet|
-            unless heartbeat_droplets.include? id
-              dea_droplets.delete(id)
+          dea_droplets.each_pair do |droplet_id, droplet_data|
+            droplet_data['instances'].each_pair do |instance_id, instance_data|
+                unless heartbeat_droplet_instances.include? instance_id
+                  droplet_data['instances'].delete(instance_id)
+                end
+            end
+            if droplet_data['instances'].size == 0
+              dea_droplets.delete(droplet_id)
             end
           end
         end
