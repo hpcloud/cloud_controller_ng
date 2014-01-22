4c5e26da9be43c504a9447d31f8509db3ab588a0
Greg Baguley <gregb@activestate.com>
2014-01-23 11:15:15 +1300

    Make an application zone default to 'default'

--- db/migrations/20140117143320_add_distribution_zone_to_application.rb
+++ db/migrations/20140117143320_add_distribution_zone_to_application.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   change do
     alter_table :apps do
-      add_column :distribution_zone, String, :default => nil
+      add_column :distribution_zone, String, :default => 'default'
       add_index :distribution_zone
     end
   end
