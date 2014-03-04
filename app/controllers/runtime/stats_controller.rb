2f95081cba0356850dc21e29301f487b2d11ec14
Tyler Power <tylerpowerx@gmail.com>
2014-03-05 12:19:51 +1300

    ensure /v2/apps/:app_guid/stats api returns ints for mem and disk usage to stay compatible with GO client

--- app/controllers/runtime/stats_controller.rb
+++ app/controllers/runtime/stats_controller.rb
@@ -7,6 +7,12 @@ module VCAP::CloudController
     def stats(guid, opts = {})
       app = find_guid_and_validate_access(:read, guid)
       stats = DeaClient.find_stats(app, opts)
+
+      stats.each do |instance_index, data|
+        data[:stats]["usage"]["mem"] =  data[:stats]["usage"]["mem"].to_i
+        data[:stats]["usage"]["disk"] =  data[:stats]["usage"]["disk"].to_i
+      end
+
       [HTTP::OK, Yajl::Encoder.encode(stats)]
     end
   end
