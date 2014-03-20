4609fecd7d67627ed2487ebf22e6a67b24897fe2
ericpromislow <ericp@activestate.com>
2014-03-20 14:16:43 -0700

    Zipbomb detection: count the actual sizes for each file in zip archives
    This is because the directory in zip files can truncate the size
    of each uncompressed member file to the low 32bits.
    
    https://bugs.activestate.com/show_bug.cgi?id=102802
    JIRA https://activestate.atlassian.net/browse/STO-329
    
    (cherry picked from commit dd7b1b6089c895c03ce3de188d84f57823105be7)

--- lib/cloud_controller/blobstore/local_app_bits.rb
+++ lib/cloud_controller/blobstore/local_app_bits.rb
@@ -26,6 +26,17 @@ class LocalAppBits
     @logger || Steno.logger("cc.local_app_bits")
   end
   
+  def self.get_disk_limit
+    disk_limit_mb = Kato::Config.get("dea_ng", "staging/disk_limit_mb")
+    disk_limit = (disk_limit_mb.to_i rescue 0) * 1024 * 1024
+    raise VCAP::CloudController::Errors::StackatoNoConfigForComponent.new("dea_ng::/staging/disk_limit_mb") if disk_limit == 0
+    disk_limit
+  end
+  
+  def self.get_check_zipfile_actual_contents_size
+    Kato::Config.get("cloud_controller", "staging/check_zipfile_actual_contents_size") || true
+  end
+  
   def self.check_compressed_bits(compressed_bits_path)
     begin
       zf = Zip::File.new(compressed_bits_path)
@@ -33,14 +44,37 @@ class LocalAppBits
       logger.debug("Failed to open zipfile on #{compressed_bits_path}: #{$!}")
       return
     end
+    # Stackato bug https://bugs.activestate.com/show_bug.cgi?id=102802
+    # The reported sizes of large files in zip files built without the Zip64 extension
+    # are truncated to the lowest 32 bits
+    #
+    # So actually open the file, and count the # of bytes in each file entry,
+    # complaining if we pass the limit.  It means reading the zipfile twice, but
+    # avoids writing large bits of data to disk.
+    check_zipfile_actual_contents_size = get_check_zipfile_actual_contents_size
+    disk_limit = get_disk_limit
+    read_block_size = 8 * 1024 * 1024
     total_size = 0
-    disk_limit_mb = Kato::Config.get("dea_ng", "staging/disk_limit_mb")
-    disk_limit = (disk_limit_mb.to_i rescue 0) * 1024 * 1024
-    raise VCAP::CloudController::Errors::StackatoNoConfigForComponent.new("dea_ng::/staging/disk_limit_mb") if disk_limit == 0
     zf.each do |entry|
-      total_size += entry.size
-      if total_size > disk_limit
-        raise VCAP::Errors::AppPackageInvalid, "Package may not be larger than #{disk_limit} bytes"
+      if entry.file? && !entry.symlink?
+        size = entry.size # reported size (or actual size % 4GB)
+        if total_size + size > disk_limit
+          raise VCAP::Errors::AppPackageInvalid, "Package may not be larger than #{disk_limit} bytes"
+        end
+        if check_zipfile_actual_contents_size
+          actual_size = 0
+          entry.get_input_stream do |fd|
+            while data = fd.read(read_block_size)
+              actual_size += data.size
+              if total_size + actual_size > disk_limit
+                raise VCAP::Errors::AppPackageInvalid, "Package may not be larger than #{disk_limit}"
+              end
+            end
+          end
+          size = actual_size
+        end
+        total_size += size
+        # assert total_size <= disk_limit
       end
     end
   end
