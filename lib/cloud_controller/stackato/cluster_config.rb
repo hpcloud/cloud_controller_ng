06d497a6fcf7426538570050af9cf974470463a7
ericpromislow <ericp@activestate.com>
2014-04-24 16:07:21 -0700

    Licensing: Return state info in the /info API, including suggested URLs
    JIRA: https://activestate.atlassian.net/browse/STO-436

--- lib/cloud_controller/stackato/cluster_config.rb
+++ lib/cloud_controller/stackato/cluster_config.rb
@@ -4,19 +4,47 @@ module VCAP::CloudController
     # Utilities for getting config info about the cluster
     
     def self.update_license_info(info, license)
-      no_license_required = Kato::Config.get("cluster", "no_license_required")
-      free_license = Kato::Config.get("cluster", "free_license")
-      if Kato::Config.get("cluster", "license_checking") != false
-        if license.is_a?(Hash)
-          info[:license] = displayable_license_subset(license)
-        else
-          info[:license] = {}
-          default_value = no_license_required || free_license
-          if default_value
-            info[:license][:memory_limit] = default_value
-          end
+      if Kato::Config.get("cluster", "license_checking") == false
+        return
+      end
+      if license.is_a?(Hash)
+        info[:license] = displayable_license_subset(license)
+      else
+        info[:license] = {}
+        no_license_memory_limit = Kato::Config.get("cluster", "memory_limits/no_license_required")
+        if (0 + no_license_memory_limit rescue nil).nil?
+            no_license_memory_limit = Kato::Constants::MEMORY_LIMIT_NO_LICENSE_REQUIRED
         end
-        info[:license][:memory_in_use] = display_memory_in_gb(memory_in_use)
+        info[:license][:memory_limit] = no_license_memory_limit
+      end
+      info[:license][:memory_in_use] = display_memory_in_gb(memory_in_use)
+      state, url = get_memory_usage_info(license, info[:license][:memory_in_use], info[:license][:memory_limit])
+      info[:license][:state] = state
+      info[:license][:url] = url if url
+    end
+    
+    def self.get_memory_usage_info(license, memory_in_use, memory_limit)
+      if memory_in_use <= memory_limit
+        [!license.is_a?(Hash) ?
+          Kato::Constants::LICENSE_STATE_NO_LICENSE_COMPLIANT :
+           Kato::Constants::LICENSE_STATE_HAS_LICENSE_COMPLIANT, nil]
+      else
+        free_license_memory_limit = Kato::Config.get("cluster", "memory_limits/free_license")
+        if (0 + free_license_memory_limit rescue nil).nil?
+            free_license_memory_limit = Kato::Constants::MEMORY_LIMIT_FREE_LICENSE
+        end
+        if !license.is_a?(Hash)
+          if memory_in_use <= free_license_memory_limit
+            [Kato::Constants::LICENSE_STATE_NO_LICENSE_NONCOMPLIANT_UNDER_FREE_MEMORY,
+             Kato::Constants::LICENSE_URL_FREE_LICENSE_URL]
+          else
+            [Kato::Constants::LICENSE_STATE_NO_LICENSE_NONCOMPLIANT_OVER_FREE_MEMORY,
+             Kato::Constants::LICENSE_URL_PURCHASE_LICENSE_URL]
+          end
+        else
+          [Kato::Constants::LICENSE_STATE_HAS_LICENSE_NONCOMPLIANT,
+           Kato::Constants::LICENSE_URL_UPGRADE_LICENSE_URL]
+        end
       end
     end
     
@@ -33,7 +61,7 @@ module VCAP::CloudController
     end
     
     def self.displayable_license_subset(license)
-        license.slice("organization", "serial", "memory_limit")
+      license.slice("organization", "serial", "memory_limit").symbolize_keys
     end
     
   end
--- spec/controllers/stackato/license_info_spec.rb
+++ spec/controllers/stackato/license_info_spec.rb
@@ -3,11 +3,25 @@ require 'digest/sha1'
 
 module VCAP::CloudController
   # Test license handling
+  OneMB = 2 ** 20
   def hashed_license_from_hash(license_hash)
     lic_parts = [:organization, :serial, :type, :memory_limit,
                  :expiration].map{|k| license_hash[k]}.join(",")
     lic_parts + "," + Digest::SHA1.hexdigest(lic_parts)
   end
+  if Kato::Config.get("cluster", "license_urls/get_free_license").nil?
+    Kato::Config.set("cluster", "license_urls/get_free_license",
+                     Kato::Constants::LICENSE_URL_FREE_LICENSE_URL)
+  end
+  if Kato::Config.get("cluster", "license_urls/upgrade_license").nil?
+    Kato::Config.set("cluster", "license_urls/upgrade_license",
+                     Kato::Constants::LICENSE_URL_UPGRADE_LICENSE_URL)
+  end
+  if Kato::Config.get("cluster", "license_urls/purchase_license").nil?
+    Kato::Config.set("cluster", "license_urls/purchase_license",
+                     Kato::Constants::LICENSE_URL_PURCHASE_LICENSE_URL)
+  end
+  
   describe VCAP::CloudController::LegacyInfo, type: :controller do
     let(:current_user) { make_user_with_default_space(:admin => true) }
     let(:headers) { headers_for(current_user) }
@@ -31,6 +45,7 @@ module VCAP::CloudController
           Kato::Config.set("cluster", "free_license", free_license)
         end
       end
+
       describe "for a user with a full license" do
         [true, nil].each do | license_checking_status|
           before do
@@ -47,19 +62,19 @@ module VCAP::CloudController
           end
           it "should return full license info" do
             get "/info", {}, headers
-            expect(last_response.status).to be(200)
+            expect(last_response.status).to eq(200)
             hash = Yajl::Parser.parse(last_response.body)
-            $stderr.puts("/info => #{hash}")
             has_license = Kato::Config.get("cluster", "license_checking")
-            expect(has_license).to be(true)
+            expect(has_license).to eq(true)
             hash.should have_key("license")
             license = hash["license"]
             license.should_not have_key("user")
             license.should have_key("organization")
             license.should have_key("memory_limit")
-            expect(license["memory_limit"]).to be(25)
+            expect(license["memory_limit"]).to eq(25)
             license.should have_key("memory_in_use")
             license.should_not have_key("expiration")
+            license.should have_key("state")
           end
         end
       end
@@ -70,19 +85,21 @@ module VCAP::CloudController
           Kato::Config.set("cluster", "license_checking", true)
           Kato::Config.set("cluster", "no_license_required", 4)
           Kato::Config.set("cluster", "free_license", 20)
+          Kato::Config.set("cluster", "free_license", 20)
         end
         it "should return empty license info" do
           get "/info", {}, headers
-          expect(last_response.status).to be(200)
+          expect(last_response.status).to eq(200)
           hash = Yajl::Parser.parse(last_response.body)
           has_license = Kato::Config.get("cluster", "license_checking")
-          expect(has_license).to be(true)
+          expect(has_license).to eq(true)
           hash.should have_key("license")
           license = hash["license"]
           license.should_not have_key("user")
           license.should_not have_key("organization")
           license.should have_key("memory_limit")
           license.should have_key("memory_in_use")
+          license.should have_key("state")
           license.should_not have_key("expiration")
         end
       end
@@ -102,12 +119,122 @@ module VCAP::CloudController
         end
         it "should return no license info" do
           get "/info", {}, headers
-          expect(last_response.status).to be(200)
+          expect(last_response.status).to eq(200)
           hash = Yajl::Parser.parse(last_response.body)
           hash.should_not have_key("license")
         end
       end
-      
+
+      describe "for a compliant license" do
+        before do
+          new_license = {
+            user: "Flip <flip@example.com",
+            organization: "example.com",
+            serial: "ABCD1234",
+            type: "paid",
+            memory_limit: 25,
+            expiration: "2099-12-31",
+          }
+          Kato::Config.set('cluster', 'license', hashed_license_from_hash(new_license), :force => true)
+          Kato::Config.set("cluster", "license_checking", true)
+          Kato::Config.set("cluster", "memory", {
+            "192.168.68.82" => 8 * OneMB,
+            "192.168.68.98" => 8 * OneMB,
+            "192.168.68.65" => 9 * OneMB})
+        end
+        it "should be compliant" do
+          get "/info", {}, headers
+          expect(last_response.status).to eq(200)
+          hash = Yajl::Parser.parse(last_response.body)
+          license = hash["license"]
+          expect(license["state"]).to eq("HAS_LICENSE_COMPLIANT")
+          license.should_not have_key("url")
+        end
+      end
+
+      describe "for a non-compliant license" do
+        before do
+          new_license = {
+            user: "Flip <flip@example.com",
+            organization: "example.com",
+            serial: "ABCD1234",
+            type: "paid",
+            memory_limit: 25,
+            expiration: "2099-12-31",
+          }
+          Kato::Config.set('cluster', 'license', hashed_license_from_hash(new_license), :force => true)
+          Kato::Config.set("cluster", "license_checking", true)
+          Kato::Config.set("cluster", "memory", {
+            "192.168.68.82" => 8 * OneMB,
+            "192.168.68.98" => 8 * OneMB,
+            "192.168.68.65" => 11 * OneMB})
+        end
+        it "should offer to upgrade the license" do
+          get "/info", {}, headers
+          expect(last_response.status).to eq(200)
+          hash = Yajl::Parser.parse(last_response.body)
+          license = hash["license"]
+          expect(license["state"]).to eq("HAS_LICENSE_NONCOMPLIANT")
+          expect(license["url"]).to eq(Kato::Config.get("cluster", "license_urls/upgrade_license"))
+        end
+      end
+
+      describe "for a compliant non-license" do
+        before do
+          Kato::Config.del('cluster', 'license')
+          Kato::Config.set("cluster", "license_checking", true)
+          Kato::Config.set("cluster", "memory", {
+            "192.168.68.82" => 2 * OneMB,
+            "192.168.68.65" => 2 * OneMB})
+        end
+        it "should be compliant" do
+          get "/info", {}, headers
+          expect(last_response.status).to eq(200)
+          hash = Yajl::Parser.parse(last_response.body)
+          license = hash["license"]
+          expect(license["state"]).to eq("NO_LICENSE_COMPLIANT")
+          license.should_not have_key("url")
+        end
+      end
+
+      describe "for a non-compliant non-license within free bounds" do
+        before do
+          Kato::Config.del('cluster', 'license')
+          Kato::Config.set("cluster", "license_checking", true)
+          Kato::Config.set("cluster", "memory", {
+            "192.168.68.82" => 5 * OneMB,
+            "192.168.68.98" => 6 * OneMB,
+            "192.168.68.65" => 9 * OneMB})
+        end
+        it "should be non-compliant but qualifies for free license" do
+          get "/info", {}, headers
+          expect(last_response.status).to eq(200)
+          hash = Yajl::Parser.parse(last_response.body)
+          license = hash["license"]
+          expect(license["state"]).to eq("NO_LICENSE_NONCOMPLIANT_UNDER_FREE_MEMORY")
+          expect(license["url"]).to eq(Kato::Config.get("cluster", "license_urls/get_free_license"))
+        end
+      end
+
+      describe "for a non-compliant non-license exceeding free bounds" do
+        before do
+          Kato::Config.del('cluster', 'license')
+          Kato::Config.set("cluster", "license_checking", true)
+          Kato::Config.set("cluster", "memory", {
+            "192.168.68.82" => 5 * OneMB,
+            "192.168.68.98" => 8 * OneMB,
+            "192.168.68.65" => 9 * OneMB})
+        end
+        it "should be non-compliant and needs a paid license" do
+          get "/info", {}, headers
+          expect(last_response.status).to eq(200)
+          hash = Yajl::Parser.parse(last_response.body)
+          license = hash["license"]
+          expect(license["state"]).to eq("NO_LICENSE_NONCOMPLIANT_OVER_FREE_MEMORY")
+          expect(license["url"]).to eq(Kato::Config.get("cluster", "license_urls/purchase_license"))
+        end
+      end
+
     end
 
   end
