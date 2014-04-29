d05cd1a873b3285e8b8362fcfa7e8f9c82ca139f
Daniil Kulchenko <daniil@kulchenko.com>
2014-04-29 13:21:38 -0700

    Update controller and access

--- app/access/app_version_access.rb
+++ app/access/app_version_access.rb
@@ -1,4 +1,10 @@
 module VCAP::CloudController
   class AppVersionAccess < BaseAccess
+    def create?(version)
+      super || version.app.space.developers.include?(context.user)
+    end
+
+    alias :update? :create?
+    alias :delete? :create?
   end
 end
--- app/controllers/runtime/app_versions_controller.rb
+++ app/controllers/runtime/app_versions_controller.rb
@@ -2,10 +2,14 @@ module VCAP::CloudController
   class AppVersionsController < RestController::ModelController
     define_attributes do
       to_one    :app
+      to_one    :droplet
       attribute :version_guid, String
+      attribute :droplet_hash, String
+      attribute :version_count, Integer
+      attribute :description, String
     end
 
-    query_parameters :app_guid, :version_guid
+    query_parameters :app_guid, :version_guid, :description, :instances, :version_count, :droplet_hash
 
     def delete(guid)
       do_delete(find_guid_and_validate_access(:delete, guid))
