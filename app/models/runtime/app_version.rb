aae9b9e031d2096fdf4105781f6c21084e3baf89
Daniil Kulchenko <daniil@kulchenko.com>
2014-04-29 13:19:25 -0700

    More fields

--- app/models/runtime/app_version.rb
+++ app/models/runtime/app_version.rb
@@ -1,12 +1,15 @@
 module VCAP::CloudController
   class AppVersion < Sequel::Model
     many_to_one :app
+    many_to_one :droplet
 
-    export_attributes :app_guid, :version_guid
-    import_attributes :app_guid, :version_guid
+    export_attributes :app_guid, :version_guid, :version_count, :droplet_hash, :description, :instances
+    import_attributes :app_guid, :version_guid, :version_count, :droplet_hash, :description, :instances
 
     def validate
       validates_presence :app
+      validates_presence :droplet
+      validates_presence :version_count
       validates_presence :version_guid
     end
 
--- db/migrations/20140428124500_add_app_versions.rb
+++ db/migrations/20140428124500_add_app_versions.rb
@@ -3,12 +3,18 @@ Sequel.migration do
     create_table :app_versions do
       VCAP::Migration.common(self)
 
-      Integer :app_id, :null => false
-      String  :version_guid, :null => false
+      Integer  :app_id, :null => false
+      String   :version_guid, :null => false
+      Integer  :version_count, :null => false
+      String   :droplet_id, :null => false
+      String   :description, :default => ""
+
+      Integer  :instances, :default => 0
 
       index :app_id
 
       foreign_key [:app_id], :apps, :name => :fk_app_versions_app_id
+      foreign_key [:droplet_id], :droplets, :name => :fk_app_versions_droplet_id
     end
   end
 end
