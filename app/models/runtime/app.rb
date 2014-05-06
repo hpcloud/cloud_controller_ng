hpcloud/cloud_controller_ng

commit 5beca9993598c213051ec53dd4e594057dc9b6bd

Don't create new versions in respond to HM autoscaling events

app/models/runtime/app.rb
@@ -90,6 +90,7 @@ module VCAP::CloudController
 
     attr_accessor :version_description
     attr_accessor :version_updated
+    attr_accessor :force_no_snapshot
 
     # Last staging response which will contain streaming log url
     attr_accessor :last_stager_response
@@ -193,6 +194,11 @@ module VCAP::CloudController
     end
 
     def snapshot_new_version
+      if app.force_no_snapshot
+        app.force_no_snapshot = false
+        return
+      end
+
       VCAP::CloudController::AppVersion.make_new_version(self)
       VCAP::CloudController::AppVersion.prune_old_versions
     end

app/controllers/runtime/apps_controller.rb
@@ -95,7 +95,7 @@ module VCAP::CloudController
       [ HTTP::NO_CONTENT, nil ]
     end
     
-    def adjust_instances(app_guid, payload)
+    def adjust_instances(app_guid, payload, force_no_version=false)
       instances = payload[:instances]
       if instances && instances.kind_of?(Array) && instances.size == 2
         before_count = instances[0]
@@ -127,6 +127,7 @@ module VCAP::CloudController
       model.db.transaction(savepoint: true) do
         app.lock!
         app.update_from_hash(payload)
+        app.force_no_snapshot = force_no_version if force_no_version
         app.save
       end
       if app.dea_update_pending?

lib/cloud_controller/health_manager_respondent.rb
@@ -93,7 +93,7 @@ module VCAP::CloudController
       factory = CloudController::ControllerFactory.new({}, logger, {}, decoded_msg, {})
       controller = factory.create_controller(VCAP::CloudController::AppsController)
       app_guid = decoded_msg.delete(:guid)
-      controller.adjust_instances(app_guid, decoded_msg)
+      controller.adjust_instances(app_guid, decoded_msg, true) # force no version creation
     end
       
     def process_request_autoscaling_settings(decoded_msg)

commit e726f5e5a335134bf7db8c4f536b8fe20f52e669

workaround regression in 5beca9993598c213051ec53dd4e594057dc9b6bd
https://bugs.activestate.com/show_bug.cgi?id=103858#c2

app/models/runtime/app.rb
@@ -194,8 +194,8 @@ module VCAP::CloudController
     end
 
     def snapshot_new_version
-      if app.force_no_snapshot
-        app.force_no_snapshot = false
+      if force_no_snapshot
+        force_no_snapshot = false
         return
       end
 