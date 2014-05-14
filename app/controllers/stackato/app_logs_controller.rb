01be0184c9fe4443a614bc32ea1fbe557466cc54
Sridhar Ratnakumar <sridharr@activestate.com>
2014-05-13 19:43:22 -0700

    stackato_logs: add "as_is" mode
    as_is sends the raw data from the logyard stream back to the client
    without any special formatting.
    
    used by applog_endpoint app.
    
    stackato_logs API will eventually go away.

--- app/controllers/stackato/app_logs_controller.rb
+++ app/controllers/stackato/app_logs_controller.rb
@@ -7,7 +7,7 @@ module VCAP::CloudController
     def get_app_logs(guid)
       app = find_guid_and_validate_access(:read, guid)
       begin
-        loglines = StackatoAppLogsClient.fetch_app_loglines(app, params["num"].to_i, params["raw"])
+        loglines = StackatoAppLogsClient.fetch_app_loglines(app, params["num"].to_i, params["raw"], params["as_is"])
       rescue Redis::CannotConnectError => e
         logger.error(e.message)
         raise Errors::StackatoAppLogServerNotReachable.new
@@ -16,7 +16,11 @@ module VCAP::CloudController
         # monolith return is used by the Web UI
         Yajl::Encoder.encode({:lines => loglines})
       else
-        loglines.map {|line| Yajl::Encoder.encode(line)}.join("\n") + "\n"
+        if params["as_is"]
+          loglines.map {|line| line}.join("\n") + "\n"
+        else
+          loglines.map {|line| Yajl::Encoder.encode(line)}.join("\n") + "\n"
+        end
       end
     end
 
--- lib/cloud_controller/stackato/app_logs_client.rb
+++ lib/cloud_controller/stackato/app_logs_client.rb
@@ -18,7 +18,7 @@ module VCAP::CloudController
       AppLogsRedisClient.redis &block
     end
 
-    def self.fetch_app_loglines(app, num=25, raw=false)
+    def self.fetch_app_loglines(app, num=25, raw=false, as_is=false)
       key = "apptail.#{app.guid}"
       num = 25 unless num > 0
 
@@ -27,32 +27,37 @@ module VCAP::CloudController
       loglines = redis { |r| r.lrange(key, 0, num-1) }
 
       loglines.reverse.map do |record|
-        record = Yajl::Parser.parse(record)
-        timestamp = record['unix_time']
-        source = record['source']
-        filename = record['filename']
-        idx = record['instance_index']
-        text = record['text']
-        nodeid = record['node_id']
-        # XXX: `raw` should be part of the logyard record. we
-        # shouldn't have to deal with formatting at all.
-        if raw
-          date = Time.at(timestamp).strftime "%Y-%m-%dT%H:%M:%S%z"
-          if idx >= 0
-            prefix = "#{app.name}[#{source}.#{idx}]"
-          else
-            prefix = "#{app.name}[#{source}]"
-          end
-          "#{date} #{prefix}: #{text}"
+        if as_is
+          # pass content from logyard stream as-is.
+          record
         else
-          {
-            :text => text,
-            :source => source,
-            :filename => filename,
-            :instance => idx,
-            :timestamp => timestamp,
-            :nodeid => nodeid
-          }
+          record = Yajl::Parser.parse(record)
+          timestamp = record['unix_time']
+          source = record['source']
+          filename = record['filename']
+          idx = record['instance_index']
+          text = record['text']
+          nodeid = record['node_id']
+          # XXX: `raw` should be part of the logyard record. we
+          # shouldn't have to deal with formatting at all.
+          if raw
+            date = Time.at(timestamp).strftime "%Y-%m-%dT%H:%M:%S%z"
+            if idx >= 0
+              prefix = "#{app.name}[#{source}.#{idx}]"
+            else
+              prefix = "#{app.name}[#{source}]"
+            end
+            "#{date} #{prefix}: #{text}"
+          else
+            {
+              :text => text,
+              :source => source,
+              :filename => filename,
+              :instance => idx,
+              :timestamp => timestamp,
+              :nodeid => nodeid
+            }
+          end
         end
       end
     end
