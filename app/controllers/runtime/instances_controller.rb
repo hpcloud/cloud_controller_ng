4b1b92e65aab2318eac7de512bec962fb7ed1128
Sridhar Ratnakumar <sridharr@activestate.com>
2014-05-14 18:45:12 -0700

    resolve instances_controller.rb
    commenting out the raise'ing of exception as before.
    
    daniil says: "NotStaged was bringing down the CC, while by API the
    correct response is an empty hash of instances."
    
    https://docs.google.com/a/activestate.com/spreadsheets/d/13aJZNT-Cos9oTAvh9cwt6IGFN-0CVS6V2SG8s9Pm5SE/edit#gid=0

--- app/controllers/runtime/instances_controller.rb
+++ app/controllers/runtime/instances_controller.rb
@@ -10,11 +10,8 @@ module VCAP::CloudController
       if app.staging_failed?
         raise VCAP::Errors::ApiError.new_from_details("StagingError", "cannot get instances since staging failed")
       elsif app.pending?
-<<<<<<< HEAD
         return Yajl::Encoder.encode({})
-        #raise VCAP::Errors::NotStaged
-=======
-        raise VCAP::Errors::ApiError.new_from_details("NotStaged")
+        # raise VCAP::Errors::ApiError.new_from_details("NotStaged")
       end
 
       if app.stopped?
@@ -22,7 +19,6 @@ module VCAP::CloudController
         msg << " as the app is in stopped state."
 
         raise VCAP::Errors::ApiError.new_from_details("InstancesError", msg)
->>>>>>> upstream/master
       end
 
       instances = DeaClient.find_all_instances(app)
