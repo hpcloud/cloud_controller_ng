548d77a9f138a70ff28c9627aa24fc977b372571
ericpromislow <ericp@activestate.com>
2014-05-27 10:07:35 -0700

    Checking for license and created admins: Here's how to do a negative test in Sequel.
    JIRA: https://activestate.atlassian.net/browse/STO-650
    Bugzilla: https://bugs.activestate.com/show_bug.cgi?id=104013

--- lib/cloud_controller/stackato/license_helper.rb
+++ lib/cloud_controller/stackato/license_helper.rb
@@ -3,12 +3,10 @@ module VCAP::CloudController
   class StackatoLicenseHelper
     def self.get_license_accepted(license)
       return false if license.blank?
-      #users = User.select(:username).
-      #             where(:admin => true, :active => true, !:username => nil)...
-      #XXX How do we talk about non-nil in pure Sequel query syntax?
-      return !! User.select(:username).
-                     where(:admin => true, :active => true).
-                     find {|user| !user.username.nil? }
+      return User.select(nil).
+                  where(:admin => true, :active => true).
+                  exclude(:username => nil).
+                  count > 0
     end
   end
 end
