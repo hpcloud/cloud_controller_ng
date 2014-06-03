a79f7edd4ebc4574d9ada06bab848d04dd106cc4
ericpromislow <ericp@activestate.com>
2014-06-03 15:22:44 -0700

    Verify droplet_id is non-null before creating a new app_version with it.
    Bugzilla: https://bugs.activestate.com/show_bug.cgi?id=104074
    JIRA: https://activestate.atlassian.net/browse/STO-672

--- db/migrations/20140602160427_create_base_app_versions.rb
+++ db/migrations/20140602160427_create_base_app_versions.rb
@@ -5,22 +5,26 @@ Sequel.migration do
       if self[:app_versions].where(:app_id => app_id).count > 0
         next
       end
-      droplet_id = self[:droplets].where(:app_id => app_id,
+      droplets = self[:droplets].where(:app_id => app_id,
                                          :droplet_hash => app[:droplet_hash]).
-                                   select(:id)
-      if droplet_id
-        version_guid = SecureRandom.uuid
-        now = Time.now
-        self[:app_versions].insert(:guid => SecureRandom.uuid,
-                                   :created_at => now,
-                                   :app_id => app_id,
-                                   :version_guid => version_guid,
-                                   :version_count => 1,
-                                   :droplet_id => droplet_id,
-                                   :description => "upgraded existing application",
-                                   :instances => app[:instances],
-                                   :memory => app[:memory])
-        from(:apps).where(:id => app_id).update(:version => version_guid)
+                                 select(:id).
+                                 to_a
+      if droplets.size > 0
+        droplet_id = droplets.first[:id]
+        if droplet_id
+          version_guid = SecureRandom.uuid
+          now = Time.now
+          self[:app_versions].insert(:guid => SecureRandom.uuid,
+                                     :created_at => now,
+                                     :app_id => app_id,
+                                     :version_guid => version_guid,
+                                     :version_count => 1,
+                                     :droplet_id => droplet_id,
+                                     :description => "upgraded existing application",
+                                     :instances => app[:instances],
+                                     :memory => app[:memory])
+          from(:apps).where(:id => app_id).update(:version => version_guid)
+        end
       end
     end
   end
