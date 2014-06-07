1af136c272b0d9276de5591d4101696b9fb37d2b
ericpromislow <ericp@activestate.com>
2014-06-06 18:21:12 -0700

    Don't complain about unpatchable versions
    JIRA: https://activestate.atlassian.net/browse/STO-693
    Bugzilla: https://bugs.activestate.com/show_bug.cgi?id=104107
    
    Only released versions have patch manifests.  So don't complain with
    '500' errors when using intermediate builds.

--- app/controllers/stackato/patch_controller.rb
+++ app/controllers/stackato/patch_controller.rb
@@ -5,16 +5,34 @@ module VCAP::CloudController
   class StackatoPatchController < RestController::Base
 
     def get_patch_status
-      KatoShell.patch_status
+      safe_get_patch_status do
+        KatoShell.patch_status
+      end
     end
 
     get "/v2/stackato/patch/status", :get_patch_status
 
     def get_patch_status_json
-      KatoShell.patch_status_json
+      safe_get_patch_status do
+        KatoShell.patch_status_json
+      end
     end
 
     get "/v2/stackato/patch/status.json", :get_patch_status_json
+
+    private
+    
+    def safe_get_patch_status
+      begin
+        yield
+      rescue VCAP::Errors::ApiError => e
+        if e.name == "ShellOutFailure" && e.message == "Shell out failed: failed to run"
+          return [].to_json
+        else
+          raise
+        end
+      end
+    end
 
   end
 end
