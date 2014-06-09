bbe7df562b39427fd765bad950b3297ccf97175c
ericpromislow <ericp@activestate.com>
2014-06-09 12:57:55 -0700

    App versions (& others): get sorted lists working again.
    JIRA: https://activestate.atlassian.net/browse/STO-692
    Bugzilla: https://bugs.activestate.com/show_bug.cgi?id=104099
    
    The upstream merge changed the param name 'order' to 'order-direction'
    (for the most part a good thing because 'order' by itself is ambiguous).
    Need to update the console as well.

--- lib/cloud_controller/rest_controller/common_params.rb
+++ lib/cloud_controller/rest_controller/common_params.rb
@@ -8,13 +8,22 @@ module VCAP::CloudController::RestController
       @logger.debug2 "parse_params: #{params}"
       # Sinatra squshes duplicate query parms into a single entry rather
       # than an array (which we might have for q)
+      if params["order-direction"].nil? && !params["order"].nil?
+        @logger.warn("query param 'order' is deprecated: use 'order-direction'")
+        params["order-direction"] = params["order"]
+      end
       res = {}
       [
         ["inline-relations-depth", Integer],
+        ["orphan-relations",       Integer ],
+        ["exclude-relations",      String  ],
+        ["include-relations",      String  ],
+        ["pretty",                 Integer ],
         ["page", Integer],
         ["results-per-page", Integer],
         ["q", String],
-        ["order-direction", String],
+        ["order-direction", String], # "asc" (default) / "desc"
+        ["order-by", String],
 
       ].each do |key, klass|
         val = params[key]
