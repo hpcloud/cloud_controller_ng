5c5ed64dc8b4002b982457717fd5ff992795095a
Tyler Power <tylerpowerx@gmail.com>
2014-07-29 10:02:30 -0700

    don't create users org or rewrite quota if org already exists. don't create users space if space already exists. use global strategy if config is missing.

--- lib/cloud_controller/security/security_context_configurer.rb
+++ lib/cloud_controller/security/security_context_configurer.rb
@@ -34,14 +34,28 @@ module VCAP::CloudController
       def create_individual_org_and_space_for_new_user(token, user)
 
         config = VCAP::CloudController::Config.config[:uaa][:new_user_strategies][:autoassign]
-        space_name = config[:space_name]
-        quota_name = config[:quota_name]
+        space_name = config[:space_name] || 'default'
+        quota_name = config[:quota_name]|| 'default'
+        user_name = token['user_name']
+        users_org = Organization.where(:name => user_name)
 
-        users_org = Organization.create(:name => token['user_name'])
-        users_org.quota_definition = QuotaDefinition.find(:name => quota_name || 'default')
+        # Create the org and enforce the quota only if it didn't already exist (allows admins to manually specialize setup)
+        unless users_org
+          users_org = Organization.create(:name => user_name)
+          users_org.quota_definition = QuotaDefinition.find(:name => quota_name)
+        end
+
+        # Add the user to the org with no roles
         users_org.add_user(user)
         users_org.save
-        users_space = Space.create(:name => space_name || 'default', :organization => users_org)
+
+        # Only create the users space if it didn't already exist
+        users_space = Space.where(:name => space_name, :organization => users_org)
+        unless users_space
+          users_space = Space.create(:name => space_name, :organization => users_org)
+        end
+
+        # The user should only be a developer in the space
         users_space.add_developer(user)
         users_space.save
       end
@@ -62,12 +76,16 @@ module VCAP::CloudController
         # Use the logged_in_at property to determine if this is the user first time logging in
         unless user.logged_in_at
           begin
-            new_user_strategy = VCAP::CloudController::Config.config[:uaa][:new_user_strategy]
+
+            # Default to the global strategy if the config is missing
+            new_user_strategy = VCAP::CloudController::Config.config[:uaa][:new_user_strategy] || 'global'
 
             if new_user_strategy == 'individual'
               create_individual_org_and_space_for_new_user(token, user)
-            else
+            elsif new_user_strategy == 'global'
               add_user_to_global_default_org_and_space(user)
+            else
+              raise "Unrecognized new user strategy #{new_user_strategy}"
             end
           rescue => e
             @logger.error("Unable to assign user '#{token['user_name']}' to default org and space using strategy '#{new_user_strategy}': #{e.message}")
