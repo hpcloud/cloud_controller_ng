db5023e0ba667e03e207ad388c7c9c4d8f29a25d
Tyler Power <tylerpowerx@gmail.com>
2014-07-29 17:03:52 -0700

    update default org/space tests to check user permissions and edge cases

--- spec/integration/default_org_and_space_spec.rb
+++ spec/integration/default_org_and_space_spec.rb
@@ -12,10 +12,6 @@ describe 'Cloud Controller', type: :integration, non_transactional: true do
     stop_nats
   end
 
-  # TODO
-  # Should only be 'user' in org and 'developer' in space (global and individual)
-  # Individual should still work if org and/or space exist already
-
   it 'adds new user to global org and space' do
 
     # Configure global orgs and spaces
@@ -42,6 +38,17 @@ describe 'Cloud Controller', type: :integration, non_transactional: true do
     expect(user.guid).to eq(user_id)
     expect(user.organizations.map { |o| o.guid }).to include(org.guid)
     expect(user.spaces.map { |s| s.guid }).to include(space.guid)
+
+    # User should not have any roles in the org, they should just be a basic user
+    expect(org.managers.map { |o| o.guid }).not_to include(user.guid)
+    expect(org.billing_managers.map { |o| o.guid }).not_to include(user.guid)
+    expect(org.auditors.map { |o| o.guid }).not_to include(user.guid)
+    expect(org.users.map { |o| o.guid }).to include(user.guid)
+
+    # User should just be a developer in the space, nothing else
+    expect(space.managers.map { |s| s.guid }).not_to include(user.guid)
+    expect(space.auditors.map { |s| s.guid }).not_to include(user.guid)
+    expect(space.developers.map { |s| s.guid }).to include(user.guid)
   end
 
   it 'adds new user to individual org and space' do
@@ -60,11 +67,75 @@ describe 'Cloud Controller', type: :integration, non_transactional: true do
     end
     user = VCAP::CloudController::User[:guid => user_id]
 
-    # User should now exist and be in their individual org and space
+    # User should now exist
     config = VCAP::CloudController::Config.config[:uaa][:new_user_strategies][:individual]
     expect(user).not_to eq(nil)
     expect(user.guid).to eq(user_id)
-    expect(user.organizations.map { |o| o.name }).to include(user_name)
-    expect(user.spaces.map { |s| s.name }).to include(config[:space_name])
+
+    # Individual org and space for the user should exist
+    org = VCAP::CloudController::Organization[:name => user_name]
+    expect(org).not_to eq(nil)
+    space = VCAP::CloudController::Space[:name => config[:space_name], :organization => org]
+    expect(space).not_to eq(nil)
+
+    # User should belong to their org and space
+    expect(user).not_to eq(nil)
+    expect(user.guid).to eq(user_id)
+    expect(user.organizations.map { |o| o.guid }).to include(org.guid)
+    expect(user.spaces.map { |s| s.guid }).to include(space.guid)
+
+    # User should not have any roles in the org, they should just be a basic user
+    expect(org.managers.map { |o| o.guid }).not_to include(user.guid)
+    expect(org.billing_managers.map { |o| o.guid }).not_to include(user.guid)
+    expect(org.auditors.map { |o| o.guid }).not_to include(user.guid)
+    expect(org.users.map { |o| o.guid }).to include(user.guid)
+
+    # User should just be a developer in the space, nothing else
+    expect(space.managers.map { |s| s.guid }).not_to include(user.guid)
+    expect(space.auditors.map { |s| s.guid }).not_to include(user.guid)
+    expect(space.developers.map { |s| s.guid }).to include(user.guid)
+  end
+
+  it 'adds new user to individual org and space when org and space already exist' do
+
+    # Configure individual orgs and spaces
+    run_cmd('/home/stackato/bin/kato config set cloud_controller_ng uaa/new_user_strategy individual')
+    stop_cc
+    start_cc
+
+    # Create the users individual org and space
+    user_id = Sham.guid
+    user_name = 'test_user'
+    config = VCAP::CloudController::Config.config[:uaa][:new_user_strategies][:individual]
+    org = VCAP::CloudController::Organization.create(:name => user_name).save
+    space = VCAP::CloudController::Space.create(:name => config[:space_name], :organization => org).save
+
+    # Access the cc api as a 'new' user for the first time
+    authorized_token = {'Authorization' => "bearer #{user_token(user_id, user_name)}"}
+    make_get_request('/v2/info', authorized_token).tap do |r|
+      r.code.should == '200'
+    end
+    user = VCAP::CloudController::User[:guid => user_id]
+
+    # User should now exist
+    expect(user).not_to eq(nil)
+    expect(user.guid).to eq(user_id)
+
+    # User should belong to their org and space
+    expect(user).not_to eq(nil)
+    expect(user.guid).to eq(user_id)
+    expect(user.organizations.map { |o| o.guid }).to include(org.guid)
+    expect(user.spaces.map { |s| s.guid }).to include(space.guid)
+
+    # User should not have any roles in the org, they should just be a basic user
+    expect(org.managers.map { |o| o.guid }).not_to include(user.guid)
+    expect(org.billing_managers.map { |o| o.guid }).not_to include(user.guid)
+    expect(org.auditors.map { |o| o.guid }).not_to include(user.guid)
+    expect(org.users.map { |o| o.guid }).to include(user.guid)
+
+    # User should just be a developer in the space, nothing else
+    expect(space.managers.map { |s| s.guid }).not_to include(user.guid)
+    expect(space.auditors.map { |s| s.guid }).not_to include(user.guid)
+    expect(space.developers.map { |s| s.guid }).to include(user.guid)
   end
 end
