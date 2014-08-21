bca2bca55a5a47362d0611256ac4604f97ee99b2
Tyler Power <tylerpowerx@gmail.com>
2014-08-21 14:20:35 +1200

    use proper api errors for app migration

--- lib/stackato/app_migration.rb
+++ lib/stackato/app_migration.rb
@@ -60,27 +60,37 @@ module VCAP::CloudController
 
       private
 
+      def app_names(migrating_app, conflicting_apps)
+        conflicting_apps.reject { |app| app == migrating_app }.map { |app| "'#{app.name}'" }.join(',')
+      end
+
       def validate_app_migration(app, domains, routes, service_instances, is_moving_org)
 
         if is_moving_org
           domains.each { |domain|
             apps = domain.routes.map { |route| route.apps }.flatten.uniq
             if apps.size != 1 || !apps.include?(app)
-              raise 'application domains are in use by more than one application'
+              raise Errors::ApiError.new_from_details(
+                        'StackatoAppMigrationValidationFailed',
+                        "Domains used by '#{app.name}' are also being used by #{app_names(app, apps)} and cannot be migrated.")
             end
           }
         end
 
         routes.each { |route|
-          if route.apps.size != 1 && !route.apps.include?(app)
-            raise 'application routes are in use by more than one application'
+          if route.apps.size != 1 || !route.apps.include?(app)
+            raise Errors::ApiError.new_from_details(
+                      'StackatoAppMigrationValidationFailed',
+                      "Routes used by '#{app.name}' are also being used by #{app_names(app, apps)} and cannot be migrated.")
           end
         }
 
         service_instances.each { |service_instance|
           apps = service_instance.service_bindings.map { |service_binding| service_binding.app }
-          if apps.size != 1 && !apps.include?(app)
-            raise 'application service instances are in use by more than one application'
+          if apps.size != 1 || !apps.include?(app)
+            raise Errors::ApiError.new_from_details(
+                      'StackatoAppMigrationValidationFailed',
+                      "Service Instances used by '#{app.name}' are also being used by #{app_names(app, apps)} and cannot be migrated.")
           end
         }
       end
