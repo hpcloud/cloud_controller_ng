4e7b6d2b7f15cfae3aecf4cd4a75e8f4f5797537
Eric Promislow <ericp@activestate.com>
2014-10-23 13:55:46 -0700

    [105335] When contacting directory-server favor the internal HTTP URI over the external one.
    Bugzilla: https://bugs.activestate.com/show_bug.cgi?id=105335
    JIRA: https://activestate.atlassian.net/browse/STO-1159
    
    Cherry-picked 448bd42dc1a2bd0841b25b0ba5715b702f7ff9c6 off 3.2

--- app/controllers/runtime/files_controller.rb
+++ app/controllers/runtime/files_controller.rb
@@ -22,18 +22,26 @@ module VCAP::CloudController
       http_response = nil
       # new VMC and new DEA, let's hand out the directory server url.
       # We sadly still have to serve the files through CC otherwise
-      if info.file_uri_v2 && opts["allow_redirect"] == true
-        uri = info.file_uri_v2
+      uri = info.file_uri_v2
+      if opts["allow_redirect"] == true
         uri = add_tail(uri) if params.include?("tail")
         return [HTTP::FOUND, {"Location" => uri}, nil]
       else
-        http_response = http_get(info.file_uri_v2, headers, nil, nil)
+        if info.host_ip && !info.host_ip.empty?
+          if (v2_port = Kato::Config.get('dea_ng', 'directory_server/v2_port'))
+            # When possible communicate directly with the DEA node's
+            # directory server and avoid re-entering the network.
+            uri.sub!(/^(http:\/\/)[a-zA-Z0-9]{32}\..*?(\/instance_paths\/.*)/, '\1' + "#{info.host_ip}:#{v2_port}" + '\2')
+          end
+        end
+        http_response = http_get(uri, headers, nil, nil)
       end
 
       unless [200, 206, 416].include? http_response.status
         msg = "Request failed for app: #{app.name}, search_param: #{search_param}"
         msg << " as there was an error retrieving the files."
 
+        logger.error("#{msg} For uri:'#{uri}', headers:#{headers} => http status #{http_response.status}")
         raise Errors::ApiError.new_from_details("FileError", msg)
       end
 
--- lib/cloud_controller/dea/dea_client.rb
+++ lib/cloud_controller/dea/dea_client.rb
@@ -23,7 +23,7 @@ module VCAP::CloudController
   end
 
   module DeaClient
-    FileUriResult ||= Class.new(Struct.new(:file_uri_v1, :file_uri_v2, :credentials)) do
+    FileUriResult ||= Class.new(Struct.new(:file_uri_v1, :file_uri_v2, :credentials, :host_ip)) do
       def initialize(opts = {})
         if opts[:file_uri_v2]
           self.file_uri_v2 = opts[:file_uri_v2]
@@ -33,6 +33,9 @@ module VCAP::CloudController
         end
         if opts[:credentials]
           self.credentials = opts[:credentials]
+        end
+        if opts[:host_ip]
+          self.host_ip = opts[:host_ip]
         end
       end
     end
@@ -342,6 +345,7 @@ module VCAP::CloudController
           result = FileUriResult.new
           if instance_found["file_uri_v2"]
             result.file_uri_v2 = instance_found["file_uri_v2"]
+            result.host_ip = instance_found["host_ip"]
           end
 
           uri_v1 = [instance_found["file_uri"], instance_found["staged"], "/", path].join("")
