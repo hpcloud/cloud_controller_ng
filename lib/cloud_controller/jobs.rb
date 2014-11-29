2ba10eb5bf2bd3b6b22a7011b84d89aa4ca3b531
Jan Dubois <jand@activestate.com>
2014-11-28 18:06:14 -0800

    [104118] Use a unique queue name for the local queue of each CC
    Kato::Local::Node.get_local_node_id returns the external IP address
    for nodes in a cluster, or 127.0.0.1 for a microcloud.
    
    (cherry picked from commit cd68815e3a83b52a60117a57f98a4185f7240ba0)

--- lib/cloud_controller/jobs.rb
+++ lib/cloud_controller/jobs.rb
@@ -10,8 +10,11 @@ require "jobs/runtime/events_cleanup"
 require "jobs/runtime/model_deletion"
 require "jobs/runtime/legacy_jobs"
 
+require "kato/local/node"
+
 class LocalQueue < Struct.new(:config)
+  NODE_ID = Kato::Local::Node.get_local_node_id
   def to_s
-    "cc-#{config[:name]}-#{config[:index]}"
+    "cc-" + NODE_ID
   end
 end
