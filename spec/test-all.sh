eda79da6988dda5decd6dbd843693b0cfec5b8b6
Ingy döt Net <ingy@activestate.com>
2015-02-08 07:55:56 -0800

    Set up to run each spec individually

--- spec/test-all.sh
+++ spec/test-all.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # PGPASSFILE content:
 # localhost:5432:cc_test:postgres:7o0j493ehp
 export DB_TEST_USER=postgres
@@ -16,4 +18,29 @@ export DB_CONNECTION="postgres://$DB_TEST_USER@$DB_TEST_HOSTNAME:$DB_TEST_PORT"
 export DB_CONNECTION_STRING="postgres://$DB_TEST_USER@$DB_TEST_HOSTNAME:$DB_TEST_PORT/$DB_TEST_DATABASE"
 export PGPASSWORD="$DB_TEST_PASSWORD"
 
-bundle exec rake spec
+# bundle exec rake spec
+
+specs=($(find spec -type f | grep '_spec\.rb$'))
+
+# Add a timer
+# Log results
+# Run in parallel
+# Add support for a list of test patterns
+for spec in "${specs[@]}"; do
+  # XXX This test hangs currently:
+  [ "$spec" != spec/unit/lib/background_job_environment_spec.rb ] ||
+    continue
+
+  file="${spec#spec/}"
+  dir=$(dirname $file)
+  mkdir -p pass/$dir
+  echo "* Running spec/$file"
+
+  rc=0
+  bundle exec rspec $spec &> pass/$file || rc=$?
+  if [[ $rc -ne 0 ]]; then
+    mkdir -p fail/$dir
+    mv pass/$file fail/$file
+    rmdir --ignore-fail-on-non-empty pass/$dir
+  fi
+done
