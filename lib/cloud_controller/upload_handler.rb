cb7d4d0144ce718e55014419bed43c0c9f93cb05
Eric Promislow <ericp@activestate.com>
2015-02-19 12:19:36 -0800

    300170 Allow config[:stackato_upload_handler] and config[:nginx] to not be hashes, this lets test spec/unit/lib/upload_handler_spec.rb:33 pass.

--- lib/cloud_controller/upload_handler.rb
+++ lib/cloud_controller/upload_handler.rb
@@ -22,11 +22,11 @@ class UploadHandler
   private
 
   def using_nginx?
-    config[:nginx][:use_nginx]
+    config[:nginx][:use_nginx] rescue false
   end
 
   def using_stackato_upload_handler?
-    config[:stackato_upload_handler][:enabled]
+    config[:stackato_upload_handler][:enabled] rescue false
   end
 
   def nginx_uploaded_file(params, resource_name)
