hpcloud/cloud_controller_ng

commit 03cc2ee68abba0821c6bcbce19c616dc564137aa

Add username property to users via sequel migration (imports usernames for existing users). Update User model to fetch and store username for new users.  Update glob style searching to support :string types.

app/controllers/runtime/users_controller.rb
@@ -18,7 +18,8 @@ module VCAP::CloudController
                      :billing_managed_organization_guid,
                      :audited_organization_guid,
                      :managed_space_guid,
-                     :audited_space_guid
+                     :audited_space_guid,
+                     :username
 
     def self.translate_validation_exception(e, attributes)
       guid_errors = e.errors.on(:guid)

app/models/runtime/user.rb
@@ -1,5 +1,8 @@
+require "uaa/scim"
+
 module VCAP::CloudController
   class User < Sequel::Model
+    include StackatoUserCreation
     no_auto_guid
 
     many_to_many      :organizations
@@ -48,9 +51,9 @@ module VCAP::CloudController
 
     default_order_by  :id
 
-    export_attributes :admin, :active, :default_space_guid, :guid
+    export_attributes :admin, :active, :default_space_guid, :guid, :username
 
-    import_attributes :guid, :admin, :active,
+    import_attributes :guid, :admin, :active, :username,
                       :organization_guids,
                       :managed_organization_guids,
                       :billing_managed_organization_guids,
@@ -63,6 +66,17 @@ module VCAP::CloudController
     def validate
       validates_presence :guid
       validates_unique   :guid
+    end
+
+    def before_save
+      super
+      cache_username
+    end
+
+    def cache_username 
+      # Cache the users username so we can provide username searching/filtering in the api
+      result = scim_client.get(:user, guid)
+      self.username = result["username"]
     end
 
     def admin?

db/migrations/20131212124700_add_username_to_users.rb
@@ -1,0 +1,40 @@
+$:.unshift(File.expand_path("../../../lib", __FILE__))
+$:.unshift(File.expand_path("../../../app", __FILE__))
+
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../../Gemfile", __FILE__)
+
+require "uaa/token_issuer"
+require "uaa/scim"
+require "kato/config"
+
+def scim_client
+  return @scim_client if @scim_client
+  external_domain = Kato::Config.get("cloud_controller_ng", 'external_domain')
+  target = "https://#{external_domain}/uaa"
+  secret = Kato::Config.get("cloud_controller_ng", 'aok/client_secret')
+  token_issuer = CF::UAA::TokenIssuer.new(target, 'cloud_controller', secret)
+  token = token_issuer.client_credentials_grant
+  @scim_client = CF::UAA::Scim.new(target, token.auth_header)
+  return @scim_client
+end
+
+Sequel.migration do
+  change do
+    alter_table(:users) do
+	    add_column :username, String, :default => nil, :case_insenstive => true
+		add_index :username
+	end
+
+	DB[:users].where(:username => nil).each do |user|
+		begin
+			guid = user[:guid]
+			result = scim_client.get(:user, guid)
+			username = result["username"]
+			DB[:users].where(:guid => guid).update(:username => username)
+			puts "Cached username for user with guid #{guid}: #{username}" 
+		rescue CF::UAA::NotFound
+			puts "User with guid #{user[:guid]} doesn't exist in AOK. Skipping."
+		end
+	end
+  end
+end

lib/vcap/rest_api/query.rb
@@ -94,16 +94,16 @@ module VCAP::RestAPI
       col_type = column_type(key)
 
       case col_type
-      when :foreign_key
-        return clean_up_foreign_key(key, val)
-      when :integer
-        val = clean_up_integer(val)
-      when :boolean
-        val = clean_up_boolean(key, val)
-      when :datetime
-        val = clean_up_datetime(val)
-      when :citext
-        val, glob = clean_up_string(val)
+        when :foreign_key
+          return clean_up_foreign_key(key, val)
+        when :integer
+          val = clean_up_integer(val)
+        when :boolean
+          val = clean_up_boolean(key, val)
+        when :datetime
+          val = clean_up_datetime(val)
+        when :string, :citext
+          val, glob = clean_up_string(val)
       end
 
       if comparison == " IN "
@@ -112,7 +112,7 @@ module VCAP::RestAPI
 
       if val.nil?
         { key => nil }
-      elsif col_type == :citext && glob
+      elsif glob
         ["#{key} LIKE ?", val]
       else
         ["#{key} #{comparison} ?", val]

commit 510bcfdae440c6444911ee567aa87be3a79fa888

300356 - Fix WebMock error resulting from User username caching
Commit 03cc2ee6 introduced username caching for the User model;
however this added a scim call on user creation which is failing
most of the tests.

Creating a StackatoHelpers module for Stackato specific mocking
and mocking out the cache_username function of User to fix this
for testing

spec/spec_helper.rb
@@ -55,6 +55,7 @@ RSpec.configure do |rspec_config|
   rspec_config.include ModelCreation
 
   rspec_config.include ServiceBrokerHelpers
+  rspec_config.include StackatoHelpers
   rspec_config.include ControllerHelpers, type: :controller, :file_path => EscapedPath.join(%w[spec unit controllers])
   rspec_config.include ControllerHelpers, type: :api
   rspec_config.include ControllerHelpers, :file_path => EscapedPath.join(%w[spec acceptance])
@@ -79,6 +80,7 @@ RSpec.configure do |rspec_config|
     TestConfig.reset
 
     stub_v1_broker
+    stub_stackato_additions
     VCAP::CloudController::SecurityContext.clear
   end
 

spec/support/stackato_helpers.rb
@@ -1,0 +1,15 @@
+# Spec support required for Stackato specific modifications
+
+module StackatoHelpers
+
+  def stub_stackato_additions
+    stub_username_caching
+  end
+
+  # Usernames are cached within cc_ng; however this requires a scim call on before_save and breaks tests
+  # See https://github.com/ActiveState/cloud_controller_ng/commit/03cc2ee68abba0821c6bcbce19c616dc564137aa
+  # for where this was introduced.
+  def stub_username_caching
+    allow_any_instance_of(VCAP::CloudController::User).to receive(:cache_username).and_return('')
+  end
+end