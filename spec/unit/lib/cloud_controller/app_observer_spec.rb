b2beecdee7ecb8d6ff044a8d9f0b77c5078259ed
Nima Kaviani <nimak@activestate.com>
2015-03-19 18:26:24 -0700

    301210 fixed the issue with Message initializer

--- spec/unit/lib/cloud_controller/app_observer_spec.rb
+++ spec/unit/lib/cloud_controller/app_observer_spec.rb
@@ -20,7 +20,7 @@ module VCAP::CloudController
     }
     let(:blobstore_url_generator) { double(:blobstore_url_generator, :droplet_download_url => "download-url") }
     let(:tps_reporter) { double(:tps_reporter) }
-    let(:diego_messenger) { Diego::Messenger.new(config_hash, message_bus, blobstore_url_generator) }
+    let(:diego_messenger) { Diego::Messenger.new(message_bus, blobstore_url_generator) }
     let(:health_manager_client) { CloudController::DependencyLocator.instance.health_manager_client }
     let(:backends) { StackatoBackends.new(config_hash, message_bus, dea_pool, stager_pool, health_manager_client) }
 
