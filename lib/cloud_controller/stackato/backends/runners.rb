8737bac880e2baaed0e16a5dcc6b6b638591377d
Eric Promislow <ericp@activestate.com>
2015-03-23 17:10:28 -0700

    301210 - Move the StackatoBackends to StackatoRunners to parallel upstream post v92
    And the code moved from stackato/backends.rb to stackato/backends/runners.rb

--- lib/cloud_controller/stackato/backends/runners.rb
+++ lib/cloud_controller/stackato/backends/runners.rb
@@ -1,26 +1,14 @@
-require "cloud_controller/backends"
+require "cloud_controller/backends/runners"
+
 module VCAP::CloudController
-  class StackatoBackends < Backends
+  class StackatoRunners < Runners
     def initialize(config, message_bus, dea_pool, stager_pool, health_manager_client)
       super(config, message_bus, dea_pool, stager_pool)
       @health_manager_client = health_manager_client
     end
-
+    
     def update_autoscaling_fields(changes)
       @health_manager_client.update_autoscaling_fields(changes)
-    end
-
-    def react_to_droplet_hash_change(app)
-      if app.started?
-        stage_if_needed(app) do |staging_result|
-          Dea::Client.start(app, :instances_to_start => app.instances) # this will temporarily cause 2x instances, allowing the HM to gracefully terminate the old ones
-          @health_manager_client.notify_of_new_live_version( :app_id => app.guid, :version => app.version )
-          broadcast_app_updated(app)
-        end
-      else
-        Dea::Client.stop(app)
-        broadcast_app_updated(app)
-      end
     end
     
     def react_to_version_change(app)
@@ -28,6 +16,19 @@ module VCAP::CloudController
       if app.started?
         stage_if_needed(app) do |staging_result|
           Dea::Client.start(app, :instances_to_start => app.instances) # this will temporarily cause 2x instances, allowing the HM to gracefully terminate the old ones
+          broadcast_app_updated(app)
+        end
+      else
+        Dea::Client.stop(app)
+        broadcast_app_updated(app)
+      end
+    end
+
+    def react_to_droplet_hash_change(app)
+      if app.started?
+        stage_if_needed(app) do |staging_result|
+          Dea::Client.start(app, :instances_to_start => app.instances) # this will temporarily cause 2x instances, allowing the HM to gracefully terminate the old ones
+          @health_manager_client.notify_of_new_live_version( :app_id => app.guid, :version => app.version )
           broadcast_app_updated(app)
         end
       else
@@ -74,4 +75,3 @@ module VCAP::CloudController
 
   end
 end
-    
