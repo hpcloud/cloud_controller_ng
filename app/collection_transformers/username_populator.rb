1b8111770461fdf6eafd7cba901aa19d4b6972dd
Eric Promislow <ericp@activestate.com>
2015-03-31 10:02:44 -0700

    301303 - When display user names, don't use the UAA-based populator, or actual names will be overwritten with nulls.

--- app/collection_transformers/username_populator.rb
+++ app/collection_transformers/username_populator.rb
@@ -7,6 +7,8 @@ module VCAP::CloudController
     end
 
     def transform(users)
+      # STACKATO: Remove this return statement when we switch from AOK to UAA
+      return users
       user_ids = users.collect(&:guid)
       username_mapping = uaa_client.usernames_for_ids(user_ids)
       users.each { |user| user.username = username_mapping[user.guid] }
