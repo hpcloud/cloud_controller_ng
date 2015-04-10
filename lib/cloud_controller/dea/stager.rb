b0a27a680e72c717430d0455b4187e181276e3c9
Ivan Sim <ivans@activestate.com>
2015-04-10 10:59:50 -0700

    301321 Add `docker_registry` argument which was missed during the upstream merge.

--- lib/cloud_controller/dea/stager.rb
+++ lib/cloud_controller/dea/stager.rb
@@ -39,7 +39,8 @@ module VCAP::CloudController
 
       def stage_app
         blobstore_url_generator = CloudController::DependencyLocator.instance.blobstore_url_generator
-        task = AppStagerTask.new(@config, @message_bus, @app, @dea_pool, @stager_pool, blobstore_url_generator)
+        docker_registry = CloudController::DependencyLocator.instance.docker_registry
+        task = AppStagerTask.new(@config, @message_bus, @app, @dea_pool, @stager_pool, blobstore_url_generator, docker_registry)
 
         @app.last_stager_response = task.stage do |staging_result|
           @runners.runner_for_app(@app).start(staging_result)
