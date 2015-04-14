98ae5a718d4a10c04369e28839610b438ca56822
Nima Kaviani <nimak@activestate.com>
2015-04-14 14:20:25 -0700

    301306 - basic test for space_usage_controller. other tests with modifications to space quota should also be added here

---
+++ spec/controllers/stackato/space_usage_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+module VCAP::CloudController
+  describe VCAP::CloudController::StackatoRolesController, type: :controller do
+
+    let(:app_obj) { AppFactory.make }
+    let(:space)   { app_obj.space }
+
+    describe "GET /v2/spaces/:id/usage" do
+      it "should return information on a space with the given id" do
+        get "/v2/spaces/#{space.guid}/usage", {}, admin_headers
+        expect(decoded_response["usage"]["mem"]).to be 0
+        expect(decoded_response["allocated"]["mem"]).to be > 0
+      end
+    end
+
+  end
+end
