7ec715d76bfd2f7ce0ebe190fdea11f118043536
Nima Kaviani <nimak@activestate.com>
2015-04-16 15:39:42 -0700

    301457 - removed tabs and comments

--- app/jobs/local_queue.rb
+++ app/jobs/local_queue.rb
@@ -1,15 +1,10 @@
 module VCAP::CloudController
   module Jobs
-    # class LocalQueue < Struct.new(:config)
-    #   def to_s
-    #     "cc-#{config[:name]}-#{config[:index]}"
-    #   end
-    # end
     class LocalQueue < Struct.new(:config)
-	  NODE_ID = Kato::Local::Node.get_local_node_id
-	  def to_s
-	    "cc-" + NODE_ID
-	  end
-	end
+    NODE_ID = Kato::Local::Node.get_local_node_id
+    def to_s
+      "cc-" + NODE_ID
+    end
+  end
   end
 end
