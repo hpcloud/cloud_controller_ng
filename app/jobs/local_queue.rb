904526cbc4c9f309c64446f2add2f8cbf9a3e0be
Ivan Sim <ivans@activestate.com>
2015-06-22 09:20:03 -0700

    301457 Add require kato/local/node dependency.

--- app/jobs/local_queue.rb
+++ app/jobs/local_queue.rb
@@ -1,10 +1,12 @@
+require 'kato/local/node'
+
 module VCAP::CloudController
   module Jobs
     class LocalQueue < Struct.new(:config)
-    NODE_ID = Kato::Local::Node.get_local_node_id
-    def to_s
-      "cc-" + NODE_ID
+      NODE_ID = Kato::Local::Node.get_local_node_id
+      def to_s
+        "cc-" + NODE_ID
+      end
     end
-  end
   end
 end
