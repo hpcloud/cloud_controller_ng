f49e2b1e3c59e106a99096277502b7f752cf8cfa
Greg Baguley <gregb@activestate.com>
2015-07-27 13:26:00 +1200

    300263 - Update StackatoLicenseHelper to use upstream username populator

--- lib/cloud_controller/stackato/license_helper.rb
+++ lib/cloud_controller/stackato/license_helper.rb
@@ -1,12 +1,19 @@
 
 module VCAP::CloudController
   class StackatoLicenseHelper
+    def self.username_populator
+      @@username_populator ||= UsernamePopulator.new(::CloudController::DependencyLocator.instance.username_lookup_uaa_client)
+    end
+
     def self.get_license_accepted(license)
       return false if license.blank?
-      return User.select(nil).
-                  where(:admin => true, :active => true).
-                  exclude(:username => nil).
-                  count > 0
+
+      # Gather the list of admin users that have a username.
+      users = User.where(:admin => true, :active => true).all
+      username_populator.transform(users)
+      users.any? do |user|
+        !user.username.nil?
+      end
     end
   end
 end
