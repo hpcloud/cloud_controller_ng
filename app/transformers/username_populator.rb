7fecc8db0f6dbf56868fd4d2b26a3f45e82a0c3b
Greg Baguley <gregb@activestate.com>
2015-07-27 13:26:23 +1200

    300263 - Revert back to upstream username transforming

--- app/transformers/username_populator.rb
+++ app/transformers/username_populator.rb
@@ -7,8 +7,6 @@ module VCAP::CloudController
     end
 
     def transform(users, opts={})
-      # STACKATO: Remove this return statement when we switch from AOK to UAA
-      return users
       user_ids = users.collect(&:guid)
       username_mapping = uaa_client.usernames_for_ids(user_ids)
       users.each { |user| user.username = username_mapping[user.guid] }
